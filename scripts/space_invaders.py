cfbe0c6d2c97fdc5447f0e474ec2825db2fc95c9
claudere <129028339+claudere0@users.noreply.github.com>
2026-07-14 01:53:05 +0500

    basic player class

--- scripts/space_invaders.py
+++ scripts/space_invaders.py
@@ -5,11 +5,49 @@ WIDTH = 512
 HEIGHT = 512
 FPS = 60
 
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load('images/player.png').convert_alpha()
+        self.width = self.image.get_width()
+        self.height = self.image.get_height()
+        self.x = (WIDTH - self.width) // 2
+        self.y = HEIGHT - self.height
+        self.rect = self.image.get_rect(topleft = (self.x, self.y))
+        self.speed = 6
+
+        self.restart()
+    
+    def restart(self):
+        pass
+
+    def get_input(self):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_RIGHT] and self.rect.x <= WIDTH - self.width:
+            self.rect.x += self.speed
+        elif keys[pygame.K_LEFT] and self.rect.x >= 0:
+            self.rect.x -= self.speed
+
+        # shoot_laser k_space
+
+    # recharge
+
+    # shoot_lazer
+    
+    def update(self):
+        self.get_input()
+        # recharge
+        # lasers.update()
+
 class Game:
     def __init__(self):
         self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
         pygame.display.set_caption('space invaders')
         self.clock = pygame.time.Clock()
+        
+        player_sprite = Player()
+        self.player = pygame.sprite.GroupSingle(player_sprite)
 
         self.running = True
 
@@ -22,11 +60,18 @@ class Game:
                     self.running = False
 
     def update(self):
+        # update objects
+        self.player.update()
+        # check collisions
+
+        # check game state
+
         pass
 
     def draw(self):
         self.screen.fill((0,0,0))
         # all game objects
+        self.player.draw(self.screen)
         pygame.display.flip()
 
     def run(self):
